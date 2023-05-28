664f3e3df873c8b32ba54d273b5112684dacdb86
karta0807913 <karta0807913@gmail.com>
2023-05-28 13:01:59 +0800

    upgrade to ubuntu22.04

--- install.bash
+++ install.bash
@@ -16,11 +16,15 @@ sudo apt-get install -y software-properties-common
 
 if ! which podman > /dev/null 2>/dev/null ; then
     set -e
-    . /etc/os-release
-    echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-    curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key \
+      | gpg --dearmor \
+      | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+        https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
+      | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
     sudo apt-get update
-    sudo apt-get -y upgrade
     sudo apt-get -y install podman
     set +e
 fi
@@ -29,9 +33,52 @@ if ! podman network inspect podman >/dev/null 2>/dev/null; then
     podman create network podman
 fi
 
+sudo add-apt-repository -y ppa:kelleyk/emacs
+curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb
+sudo apt update
+sudo apt install -y i3-wm feh maim htop /tmp/chrome.deb emacs27 playerctl rofi python3-pip numlockx xclip ibus-chewing dunst i3lock vim python3-pip git unzip acpid polybar picom libxcb-dpms0
+# the fonts for st terminal
+sudo apt install -y fonts-linuxlibertine fonts-inconsolata fonts-inconsolata fonts-emojione fonts-symbola byzanz
+
+sudo ln -f "$(which python3)" "$(dirname $(which python3))/python"
+
+pip3 install dbus-python
+
+podman rm -f i3 || echo ""
+
+podman run --name i3 ubuntu:22.04 bash -c "
+apt-get update;
+DEBIAN_FRONTEND=noninteractive apt-get install -y gcc meson pkg-config libstartup-notification0-dev libxcb1-dev libxcb-xkb-dev libxcb-randr0-dev libxcb-shape0-dev libxcb-util-dev libxcb-cursor-dev libxcb-keysyms1-dev libxcb-icccm4-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libyajl-dev libxkbcommon-x11-dev libpcre2-dev  libcairo2-dev libpango1.0-dev libglib2.0-dev libev-dev asciidoc-base ninja-build wget xz-utils libxcb-xinerama0-dev;
+wget https://i3wm.org/downloads/i3-4.22.tar.xz;
+tar -xvf i3-4.22.tar.xz;
+cd i3-4.22;
+meson build -Ddocs=true -Dmans=true;
+ninja -C build/ install;
+"
+rm -rf /tmp/i3;
+mkdir -p /tmp/i3/share /tmp/i3/bin /tmp/i3/share/man /tmp/i3/doc;
+podman cp i3:/usr/local/share/doc/i3 /tmp/i3/share/doc;
+podman cp i3:/usr/local/share/man/man1 /tmp/i3/share/man/;
+podman cp i3:/usr/local/etc/i3 /tmp/i3/etc;
+podman cp i3:/i3-4.22/build/i3 /tmp/i3/bin;
+podman cp i3:/i3-4.22/build/i3bar /tmp/i3/bin;
+podman cp i3:/i3-4.22/build/i3-config-wizard /tmp/i3/bin;
+podman cp i3:/i3-4.22/build/i3-dump-log /tmp/i3/bin;
+podman cp i3:/i3-4.22/build/i3-input /tmp/i3/bin;
+podman cp i3:/i3-4.22/build/i3-msg /tmp/i3/bin;
+podman cp i3:/i3-4.22/build/i3-nagbar /tmp/i3/bin;
+podman cp i3:/i3-4.22/i3-dmenu-desktop /tmp/i3/bin;
+podman cp i3:/i3-4.22/i3-migrate-config-to-v4 /tmp/i3/bin;
+podman cp i3:/i3-4.22/i3-save-tree /tmp/i3/bin;
+podman cp i3:/i3-4.22/i3-sensible-editor /tmp/i3/bin;
+podman cp i3:/i3-4.22/i3-sensible-pager /tmp/i3/bin;
+podman cp i3:/i3-4.22/i3-sensible-terminal /tmp/i3/bin;
+sudo cp -R /tmp/i3/* /usr/local/;
+podman rm -f i3;
+
 podman rm -f picom || echo "no picon found"
 
-podman run --name picom ubuntu:20.04 bash -c "
+podman run --name picom ubuntu:22.04 bash -c "
 apt-get update;
 DEBIAN_FRONTEND=noninteractive apt-get install -y git gcc g++ libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson;
 git clone --depth 1 https://github.com/yshui/picom;
@@ -47,7 +94,7 @@ podman rm -f picom;
 
 podman rm -f polybar || echo "no polybar found"
 
-podman run --name polybar ubuntu:20.04 bash -c "
+podman run --name polybar ubuntu:22.04 bash -c "
 apt-get update;
 DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev gcc g++ make libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libuv1-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev;
 git clone --recursive --depth 1 https://github.com/polybar/polybar.git
@@ -59,32 +106,23 @@ make -j$(nproc)
 # Optional. This will install the polybar executable in /usr/local/bin
 make install"
 
-podman cp polybar:/usr/local/bin/polybar ~/.local/bin
-podman cp polybar:/usr/local/bin/polybar-msg ~/.local/bin
+podman cp polybar:/usr/bin/polybar ~/.local/bin
+podman cp polybar:/usr/bin/polybar-msg ~/.local/bin
 
 podman rm -f polybar;
 
-sudo add-apt-repository -y ppa:kelleyk/emacs
-sudo add-apt-repository -y ppa:regolith-linux/release
-curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb
-sudo apt update
-sudo apt install -y i3-gaps-wm feh maim htop /tmp/chrome.deb emacs27 font-manager playerctl rofi python3-pip numlockx xclip ibus-chewing dunst i3lock vim libxcb-damage0 libconfig9 libxcb-composite0 libjsoncpp1 libxcb-composite0 libjsoncpp1 libmpdclient2 libnl-genl-3-200 python3-pip git make gcc g++ unzip acpid
-# the fonts for st terminal
-sudo apt install -y fonts-linuxlibertine fonts-inconsolata fonts-inconsolata fonts-emojione fonts-symbola byzanz
-
-sudo ln -f "$(which python3)" "$(dirname $(which python3))/python"
-
-pip3 install dbus-python
-
 # compile st
+podman run --name polybar ubuntu:22.04 bash -c "
+sudo apt install -y libharfbuzz-dev libx11-dev libxft-dev gcc make git
 git clone --depth 1 https://github.com/karta0807913/st /tmp/st
 cd /tmp/st
-sudo apt install -y libharfbuzz-dev libx11-dev libxft-dev
 make all
 cp ./st ~/.local/bin
 # enable the delete key
-echo "set enable-keypad on" >> ~/.inputrc
 cd -
+"
+echo "set enable-keypad on" >> ~/.inputrc
+podman cp st:/tmp/st/st ~/.local/bin/
 
 git clone --depth 1 https://github.com/karta0807913/config.git /tmp/config
 cd /tmp/config
@@ -106,23 +144,24 @@ unzip -u CascadiaCode.zip
 cd -
 
 # set font and gnome terminal theme
-font-manager -i /tmp/ttf/CascadiaCode.ttf
+mkdir ~/.local/share/fonts
+mv /tmp/ttf/CascadiaCode.ttf ~/.local/share/fonts
 fc-cache -f -v
 dconf load /org/gnome/terminal/legacy/profiles:/ < /tmp/config/gnome-terminal-profiles.dconf
 # hide menubar
 gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
 
 # add i3 configure
-cat<<EOF | sudo tee /usr/share/xsessions/i3.desktop
-[Desktop Entry]
-Name=i3
-Comment=improved dynamic tiling window manager
-Exec=i3
-TryExec=i3
-Type=Application
-X-LightDM-DesktopName=i3
-DesktopNames=i3
-Keywords=tiling;wm;windowmanager;window;manager;
-EOF
-
+# cat<<EOF | sudo tee /usr/share/xsessions/i3.desktop
+# [Desktop Entry]
+# Name=i3
+# Comment=improved dynamic tiling window manager
+# Exec=i3
+# TryExec=i3
+# Type=Application
+# X-LightDM-DesktopName=i3
+# DesktopNames=i3
+# Keywords=tiling;wm;windowmanager;window;manager;
+# EOF
+#
 echo "install finish, please login again"
